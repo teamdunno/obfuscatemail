8f3e10fdb4408df869cd72c52f81cb498624ddc6
vin(cent) <70825723+vintheweirdass@users.noreply.github.com>
2024-11-30 21:31:11 +0700

    Delete obfuscateEmail.d.ts

--- obfuscateEmail.d.ts
+++
@@ -1,6 +0,0 @@
-export = obfuscateEmail;
-declare function obfuscateEmail(email: string, options?: import('./utils').Options): string;
-declare namespace obfuscateEmail {
-    export { DEFAULT_OPTIONS };
-}
-import { DEFAULT_OPTIONS } from "./utils";
