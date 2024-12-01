e38a7a74152a2ec670c7ed84bfb05b402bfc4262
vin(cent) <70825723+vintheweirdass@users.noreply.github.com>
2024-12-01 21:13:31 +0700

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,13 +1,21 @@
 // export * from "./utils.ts"
-import {type Options, type Nullish, getOptions, DEFAULT_OPTIONS} from "./utils.ts"
-export type {Options}
-export {DEFAULT_OPTIONS}
+import {
+  DEFAULT_OPTIONS,
+  getOptions,
+  type Nullish,
+  type Options,
+} from './utils.ts';
+export type { Options };
+export { DEFAULT_OPTIONS };
 /**
  * Obfuscate an email.
  * @returns {string}
  * @throws {Error} if an option type is invalid.
  */
-export function obfuscateEmail (email:string, options?:Nullish<Options>):string {
+export function obfuscateEmail(
+  email: string,
+  options?: Nullish<Options>,
+): string {
   const opts = getOptions(options);
   if (
     typeof email !== 'string' ||
@@ -24,39 +32,38 @@ export function obfuscateEmail (email:string, options?:Nullish<Options>):string 
   const visibleCharactersStartLength =
     opts.visibleCharacters.startLength > 0 && name.length > 1
       ? Math.min(
-          name.length - opts.minimumLength > 0
-            ? name.length - opts.minimumLength
-            : 0,
-          opts.visibleCharacters.startLength,
-        )
+        name.length - opts.minimumLength > 0
+          ? name.length - opts.minimumLength
+          : 0,
+        opts.visibleCharacters.startLength,
+      )
       : 0;
 
-  const visibleCharactersEndLength =
-    opts.visibleCharacters.endLength > 0 &&
-    name.length >
-      visibleCharactersStartLength + opts.minimumLength
-      ? Math.min(
-          name.length -
-            visibleCharactersStartLength -
-            opts.minimumLength,
-          opts.visibleCharacters.endLength,
-        )
-      : 0;
+  const visibleCharactersEndLength = opts.visibleCharacters.endLength > 0 &&
+      name.length >
+        visibleCharactersStartLength + opts.minimumLength
+    ? Math.min(
+      name.length -
+        visibleCharactersStartLength -
+        opts.minimumLength,
+      opts.visibleCharacters.endLength,
+    )
+    : 0;
 
   const visibleCharactersMiddleLength =
     opts.visibleCharacters.middleLength > 0 &&
-    name.length -
-      visibleCharactersStartLength -
-      visibleCharactersEndLength -
-      opts.minimumLength >
-      0
-      ? Math.min(
-          name.length -
+      name.length -
             visibleCharactersStartLength -
             visibleCharactersEndLength -
-            opts.minimumLength,
-          opts.visibleCharacters.middleLength,
-        )
+            opts.minimumLength >
+        0
+      ? Math.min(
+        name.length -
+          visibleCharactersStartLength -
+          visibleCharactersEndLength -
+          opts.minimumLength,
+        opts.visibleCharacters.middleLength,
+      )
       : 0;
 
   const charactersLeftToObfuscate = Math.max(
@@ -64,23 +71,26 @@ export function obfuscateEmail (email:string, options?:Nullish<Options>):string 
     name.length - visibleCharactersStartLength - visibleCharactersEndLength,
   );
 
-  const middleCharacters =
-    visibleCharactersMiddleLength > 0
-      ? name
-          .substring(visibleCharactersStartLength)
-          .substring(
-            Math.round(
-              charactersLeftToObfuscate / 2 - visibleCharactersMiddleLength / 2,
-            ),
-          )
-          .substring(0, visibleCharactersMiddleLength)
-      : '';
+  const middleCharacters = visibleCharactersMiddleLength > 0
+    ? name
+      .substring(visibleCharactersStartLength)
+      .substring(
+        Math.round(
+          charactersLeftToObfuscate / 2 - visibleCharactersMiddleLength / 2,
+        ),
+      )
+      .substring(0, visibleCharactersMiddleLength)
+    : '';
 
-  return `${name.substring(0, visibleCharactersStartLength)}${'*'.repeat(
-    Math.max(1, Math.floor(opts.asterisksLength / 2)),
-  )}${middleCharacters}${'*'.repeat(
-    Math.max(1, Math.floor(opts.asterisksLength / 2)),
-  )}${
+  return `${name.substring(0, visibleCharactersStartLength)}${
+    '*'.repeat(
+      Math.max(1, Math.floor(opts.asterisksLength / 2)),
+    )
+  }${middleCharacters}${
+    '*'.repeat(
+      Math.max(1, Math.floor(opts.asterisksLength / 2)),
+    )
+  }${
     visibleCharactersEndLength > 0
       ? name.substring(name.length - visibleCharactersEndLength)
       : ''
@@ -89,6 +99,7 @@ export function obfuscateEmail (email:string, options?:Nullish<Options>):string 
       ? domainName
       : '*'.repeat(Math.max(3, opts.asterisksLength - 3))
   }.${opts.showDomainExtension ? domainExtension : '***'}`;
-};
+}
 
-export default obfuscateEmail
+export default obfuscateEmail;
+
